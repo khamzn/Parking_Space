34a95a085bd203221d0b1b9ea3d50b874747e1f8
khamzn <polyakovrrn@gmail.com>
2024-11-29 15:16:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,27 @@
 import cv2
 import pickle
 import numpy as np
+import math
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('..')
+
+value = 0.014
 
 with open('CarPos', 'rb') as f:
     posList = pickle.load(f)
 
+def square_poly(coords):
+    x1, y1 = coords[0][0][0], coords[0][0][1]
+    x2, y2 = coords[1][0][0], coords[1][0][1]
+    x3, y3 = coords[2][0][0], coords[2][0][1]
+    x4, y4 = coords[3][0][0], coords[3][0][1]
+    a = math.sqrt(abs(x2 - x1) ** 2+abs(y2-y1)**2)
+    b = math.sqrt(abs(x3 - x2) ** 2 + abs(y3 - y2) ** 2)
+    c = math.sqrt(abs(x4 - x3) ** 2 + abs(y4 - y3) ** 2)
+    d = math.sqrt(abs(x1 - x4) ** 2 + abs(y1 - y4) ** 2)
+    perimeter = a+b+c+d
+    square = math.sqrt((perimeter-a)*(perimeter-b)*(perimeter-c)*(perimeter-d))
+    return square
 
 def check_space(img, posList):
     posList = [posList[i:i + 4] for i in range(0, len(posList), 4)]
@@ -24,10 +39,10 @@ def check_space(img, posList):
             masked = cv2.bitwise_and(img, img, mask=mask)
 
             count = cv2.countNonZero(masked)
-            # cv2.putText(image, str(count), list[i][0], cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1)
-            cv2.putText(image, str(i+1), posList[i][2], cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+            cv2.putText(image, str(count/square_poly(pos)), posList[i][0], cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1)
+            #cv2.putText(image, str(i+1), posList[i][2], cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
 
-            if count < 650:
+            if count/square_poly(pos) < value:
                 color = (0, 255, 0)
                 thickness = 3
                 spaces.append(i+1)
