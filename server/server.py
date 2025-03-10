7eca20bf05411b562c4927b4762015b254351268
khamzn <polyakovrrn@gmail.com>
2025-03-10 20:16:19 +0300

    Update server.py

--- server/server.py
+++ server/server.py
@@ -3,27 +3,43 @@ import sqlite3
 
 server = Flask(__name__)
 
-
-def init_db():
-    conn = sqlite3.connect('instance/database_parking.db')
-    conn.execute('CREATE TABLE IF NOT EXISTS spaces (id INTEGER)')
+def init_db(path):
+    conn = sqlite3.connect(path)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS spaces (
+            id INTEGER PRIMARY KEY,
+            coords VARCHAR,
+            color VARCHAR,
+            status BOOLEAN
+        )
+    ''')
+    conn.commit()
     conn.close()
-
-
 
 
 @server.before_request
 def before_request():
-    init_db()
-    # insert_db(2)
-    # insert_db(4)
+    init_db('instance/database_parking.db')
+
 
 @server.route('/')
 def index():
     conn = sqlite3.connect('instance/database_parking.db')
-    free_spaces = conn.execute('SELECT * FROM spaces').fetchall()
+    rows = conn.execute('SELECT * FROM spaces').fetchall()
+    free_spaces = 0
+    colors = []
+    coords = []
+    print(rows)
+    for i in range(len(rows)):
+        if rows[i][3]:
+            free_spaces += 1
+        coords.append(rows[i][1])
+        colors.append(rows[i][2])
+
+    print(coords, colors)
     conn.close()
-    return render_template('base.html', free_spaces=free_spaces)
+    return render_template('base.html', free_spaces=free_spaces, quads=zip(coords, colors))
+
 
 server.run()
-
